326fc028b03e1c354e681cefd1167919217a6243
minhtrannhat <minhtrannhat2001@gmail.com>
2022-01-29 13:15:59 -0500

    Fix: Updated dependencies, formated Sms.py

--- backend/sms/sendSMS.py
+++ backend/sms/sendSMS.py
@@ -1,4 +1,4 @@
-from twilio.rest import Client 
+from twilio.rest import Client
 import os
 from dotenv import load_dotenv
 
@@ -7,12 +7,14 @@ load_dotenv()
 
 account_sid = os.getenv("account_sid")
 auth_token = os.getenv("auth_token")
-client = Client(account_sid, auth_token) 
+client = Client(account_sid, auth_token)
 
 
 def send(phone, message):
     print("Sending SMS to: " + phone + " with message: " + message)
-    message = client.messages.create(  messaging_service_sid='MG6fecada2030f28c4aef97e49d7b8eb3a', 
-                                            body=message, 
-                                            to=phone) 
+    message = client.messages.create(
+        messaging_service_sid="MG6fecada2030f28c4aef97e49d7b8eb3a",
+        body=message,
+        to=phone,
+    )
     print(message.sid)
