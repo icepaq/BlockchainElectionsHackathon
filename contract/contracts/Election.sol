873ac7a971ed81f5dcf636285f4cffc5885d8b85
Anton <62949848+icepaq@users.noreply.github.com>
2022-01-29 13:47:09 -0500

    delete old election contract

--- contract/contracts/Election.sol
+++ contract/contracts/Election.sol
@@ -1,64 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-contract Election  {
-    struct Candidate {
-        uint id;
+contract Election{
+
+    struct Candidate{
+        uint256 id;
         string name;
         string party;
         uint256 voteCount;
     }
 
-    address private owner;
-    constructor() {
-        owner = msg.sender;
+    struct Voter{
+        bool authorized;
+        bool voted;
+        uint vote;
     }
 
-    Candidate[] public candidatesList;
+    address private owner;
+    string private electionName;
+    mapping(address => Voter) private voters;
+    Candidate[] private candidates;
+    uint private totalVotes;
+    uint size = 3;
 
-    mapping(address => bool) public voters;
-    mapping(address => bool) public hasVoted;
-    mapping(uint => uint) public votes; // Candiate ID => Vote Count
-
-
-    modifier onlyOwner() {
+    modifier ownerOnly(){
         require(msg.sender == owner);
         _;
     }
 
-    modifier onlyApprovedVoter() {
-        require(voters[msg.sender] == true);
-        _;
+    constructor() {
+        owner = msg.sender;
+        electionName = "Canada Election 2022";
+        addCandidate(0, "Trudeau, Justin", "Liberal Party of Canada");
+        addCandidate(1, "O'Toole, Erin", "Conservative Party of Canada");
+        addCandidate(2, "Blanchet, Yves-Francois", "Block-Quebecois");
+        addCandidate(3, "Singh, Jagmeet", "New Democratic Party");
     }
 
-    function addVoter() onlyOwner public {
-        voters[msg.sender] = true;
-        hasVoted[msg.sender] = false;
+    function addCandidate(uint256 _id, string memory _name, string memory _party) ownerOnly public{
+        candidates.push(Candidate(_id, _name, _party, 0));
     }
 
-    function addCandidate(uint256 _id, string memory _name, string memory _party) onlyOwner public {
-
-        Candidate memory candidate = Candidate({
-            id: _id,
-            name: _name,
-            party: _party,
-            voteCount: 0
-        });
-
-        candidatesList.push(candidate);
+    function addVoter(address _address) ownerOnly public {
+        voters[_address].authorized = true;
     }
 
-    function getCandidatesIDs()  view public returns(Candidate[] memory){
-        return candidatesList;
+    function getNumberCandidate() public view returns(uint){
+        return candidates.length;
     }
 
-    function vote(uint256 _candidateId) onlyApprovedVoter public {
+    function getPartyCandidate() public view returns(string memory, string memory, string memory, string memory){
+        return(candidates[0].party, candidates[1].party, candidates[2].party, candidates[3].party);
+    }
 
-        // Check if voter has already voted
-        if (hasVoted[msg.sender] == true) {
-            revert();
-        }
+     function getIdCandidate() public view returns(uint256, uint256, uint256, uint256){
+        return(candidates[0].id, candidates[1].id, candidates[2].id, candidates[3].id);
+    }
+    
+    function getResults()  view public returns(uint256, uint256, uint256, uint256){
+        return(candidates[0].voteCount, candidates[1].voteCount, candidates[2].voteCount, candidates[3].voteCount);
+    }
 
-        votes[_candidateId] += 1;
+    function vote(uint _voteIndex) public {
+        require(!voters[msg.sender].voted);
+        require(voters[msg.sender].authorized);
+
+        voters[msg.sender].vote = _voteIndex;
+        voters[msg.sender].voted = true;
+
+        candidates[_voteIndex].voteCount += 1; 
+        totalVotes += 1;
     }
 }
---
+++ contract/contracts/Election_OLD.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+contract Election  {
+    struct Candidate {
+        uint id;
+        string name;
+        string party;
+        uint256 voteCount;
+    }
+
+    address private owner;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    Candidate[] public candidatesList;
+
+    mapping(address => bool) public voters;
+    mapping(address => bool) public hasVoted;
+    mapping(uint => uint) public votes; // Candiate ID => Vote Count
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyApprovedVoter() {
+        require(voters[msg.sender] == true);
+        _;
+    }
+
+    function addVoter() onlyOwner public {
+        voters[msg.sender] = true;
+        hasVoted[msg.sender] = false;
+    }
+
+    function addCandidate(uint256 _id, string memory _name, string memory _party) onlyOwner public {
+
+        Candidate memory candidate = Candidate({
+            id: _id,
+            name: _name,
+            party: _party,
+            voteCount: 0
+        });
+
+        candidatesList.push(candidate);
+    }
+
+    function getCandidatesIDs()  view public returns(Candidate[] memory){
+        return candidatesList;
+    }
+
+    function vote(uint256 _candidateId) onlyApprovedVoter public {
+
+        // Check if voter has already voted
+        if (hasVoted[msg.sender] == true) {
+            revert();
+        }
+
+        votes[_candidateId] += 1;
+    }
+}
