d546a8701fa23a30ffa83d647ba8b5cbef1faabd
Salah Warid <89282233+itsSalah@users.noreply.github.com>
2022-01-29 23:06:36 -0500

    overall changes

--- backend/visualization.py
+++ backend/visualization.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
-ls = [400,300,100,50]
+import requests
+import json
+
+response_API = requests.get('http://localhost:3000/api/getElectionResults')
+data = response_API.text
+parse_json = json.loads(data)
+ls = []
+ls.append(int(parse_json['result'][0][3]))
+ls.append(int(parse_json['result'][1][3]))
+ls.append(int(parse_json['result'][2][3]))
+ls.append(int(parse_json['result'][3][3]))
+
 sum = 0
 for i in ls:
     sum += i
 
-plt.figure(figsize=[14,10])
-plt.barh(["Liberal Party of Canada"],ls[0],label='Justin Trudeau', color = 'r')
-plt.barh(["Conservative Party of Canada"],ls[1],label="Erin O'Toole", color = 'b')
+plt.figure(figsize=[16,12])
+plt.barh(["Liberal Party"],ls[0],label='Justin Trudeau', color = 'r')
+plt.barh(["Conservative Party"],ls[1],label="Erin O'Toole", color = 'b')
 plt.barh(["Block-Quebecois"],ls[2],label='Yves-Francois Blanchet', color = '#3776ab')
 plt.barh(["New Democratic Party"],ls[3],label='Jagmeet Singh', color = 'orange')
 plt.legend()
@@ -16,7 +27,7 @@ plt.title('Canada Election 2022 on Ethereum BlockChain')
 plt.savefig('barplot.png')
 plt.clf()
 
-labels = ["Liberal Party of Canada", "Conservative Party of Canada", "Block-Quebecois", "New Democratic Party"]
+labels = ["Liberal Party", "Conservative Party", "Block-Quebecois", "New Democratic Party"]
 sizes = [ls[0]/sum*100,ls[1]/sum*100,ls[2]/sum*100,ls[3]/sum*100]
 colors = ['r', 'b', '#3776ab', 'orange']
 explode = (0.05,0.05,0.05,0.05)
